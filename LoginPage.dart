69ad860a2c814b850520807e7d849bb690e224e5
Victor Adhikary <154785811+victoradhikary@users.noreply.github.com>
2024-01-07 19:59:17 +0530

    Create LoginPage.dart

---
+++ LoginPage.dart
@@ -0,0 +1,146 @@
+Column(
+                children: [
+                            Column(
+                        children: [
+                                    Image.asset(
+                            "assets/Mesh gradient.png",
+                            width: 430,
+                            height: 944,
+                            )
+                        ],
+                    ),
+        Stack(children: [    Container(
+                            width: 357,
+                            height: 65.735595703125,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0xc4ffffff))
+                            ),
+                Text(
+                            "SEHAJ",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeith.w600,
+                            )
+                        )],),
+        Stack(children: [    Container(
+                            width: 357,
+                            height: 65.735595703125,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0xc4ffffff))
+                            ),
+                Text(
+                            "**********",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeith.w600,
+                            )
+                        )],),
+        Stack(children: [    Container(
+                            width: 357,
+                            height: 65.735595703125,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(30), 
+                        color: Colors.white)
+                            ),
+                Text(
+                            "Sign in with Google",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeith.w600,
+                            )
+                        ),
+                Column(
+                                children: [
+                                            Container(
+                                width: 40.83333206176758,
+                                height: 42.5,
+                                ),
+                        Container(
+                                width: 31.964332580566406,
+                                height: 16.4793758392334,
+                                ),
+                        Container(
+                                width: 31.86429214477539,
+                                height: 16.942623138427734,
+                                ),
+                        Container(
+                                width: 20.41666603088379,
+                                height: 20.005002975463867,
+                                )
+                                ],
+                            )],),
+        Container(
+                width: 85,
+                height: 0,
+                ),
+        Container(
+                width: 85,
+                height: 0,
+                ),
+        Text(
+                    "LOGIN",
+                    style: TextStyle(
+                        fontSize: 96,
+                        fontWeight: FontWeith.w400,
+                    )
+                ),
+        Text(
+                    "NAME",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeith.w400,
+                    )
+                ),
+        Text(
+                    "PASSWORD",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeith.w400,
+                    )
+                ),
+        Stack(children: [    Container(
+                            width: 136,
+                            height: 57,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(50), 
+                        color: Colors.white)
+                            ),
+                Text(
+                            "LOG IN",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeith.w800,
+                            )
+                        )],),
+        Text(
+                    "SIGN UP!",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeith.w800,
+                    )
+                ),
+        Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeith.w800,
+                    )
+                ),
+        Text(
+                    "Sign in to continue",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeith.w800,
+                    )
+                ),
+        Text(
+                    "OR",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeith.w400,
+                    )
+                )
+                ],
+            )
