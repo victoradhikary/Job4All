b9d169585cf603c45f5e7389fc89bb8824473c36
victoradhikary <victoradhikary2608@gmail.com>
2024-01-07 23:57:04 +0530

    otp verify design code

---
+++ otpverifi.dart
@@ -0,0 +1,106 @@
+Column(
+                children: [
+                            Column(
+                        children: [
+                                    Column(
+                                children: [
+                                            Image.asset(
+                                    "assets/Mesh gradient.png",
+                                    width: 430,
+                                    height: 944,
+                                    )
+                                ],
+                            ),
+                Stack(children: [    Container(
+                                    width: 232,
+                                    height: 57,
+                                    decoration:     BoxDecoration(
+                                borderRadius: BorderRadius.circular(50), 
+                                color: Colors.white)
+                                    ),
+                        Text(
+                                    "Proceed",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeith.w800,
+                                    )
+                                )],),
+                Text(
+                            "Verify your number via  OTP",
+                            style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeith.wSymbol(figma.mixed),
+                            )
+                        ),
+                Text(
+                            "Let’s Verify",
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeith.w800,
+                            )
+                        ),
+                Container(
+                            width: 67,
+                            height: 69,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0x00d9d9d9))
+                            ),
+                Container(
+                            width: 67,
+                            height: 69,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0x00d9d9d9))
+                            ),
+                Container(
+                            width: 67,
+                            height: 69,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0x00d9d9d9))
+                            ),
+                Container(
+                            width: 67,
+                            height: 69,
+                            decoration:     BoxDecoration(
+                        borderRadius: BorderRadius.circular(15), 
+                        color: Color(0x00d9d9d9))
+                            ),
+                Stack(children: [    Image.asset(
+                                    "assets/Ellipse 1.png",
+                                    width: 30,
+                                    height: 30,
+                                    ),
+                        Text(
+                                    "Auto Verify",
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeith.w300,
+                                    )
+                                )],),
+                Stack(children: [    Container(
+                                    width: 340,
+                                    height: 55,
+                                    decoration:     BoxDecoration(
+                                borderRadius: BorderRadius.circular(25), 
+                                color: Color(0x00d9d9d9))
+                                    ),
+                        Text(
+                                    "+91",
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeith.w400,
+                                    )
+                                ),
+                        Text(
+                                    "Enter phone number",
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeith.w400,
+                                    )
+                                )],)
+                        ],
+                    )
+                ],
+            )
